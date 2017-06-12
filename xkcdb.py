42d5c9132eb26522b4adebe4270b9682c0980c47
Kacper Daniel <kacperdev@gmail.com>
2017-06-12 22:53:28 +0100

    Update xkcdb.py

--- xkcdb.py
+++ xkcdb.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
 import gi
 import xkcd
-import math
+from random import SystemRandom
 
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
 
-comic = xkcd.Comic(91)
-comic.download(output="/tmp/",outputFile="xkcd.png")
+rand = SystemRandom()
+
+def getRandomComic():
+    number = rand.randint(1,xkcd.getLatestComicNum())
+    return xkcd.Comic(number)
+
 class myWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="XKCD Browser")
-        self.box = Gtk.VBox()
+        self.main_box = Gtk.VBox()
+
         self.image = Gtk.Image()
         self.image_area = Gtk.Box()
-        self.button = Gtk.Button(label="Click Here")
-        self.box.add(self.image_area)
-        self.box.add(self.button)
+        self.button = Gtk.Button(label="random xkcd")
+
         self.image.set_from_file('/tmp/xkcd.png')
         self.image_area.add(self.image)
         self.image_area.show_all()
-        #self.button.connect ("clicked", self.update_image)
-        self.add(self.box)
-    #def on_button_clicked (self, widget):
-    #    comic.show()
 
-    #def update_image (widged, data=None):
-    #    image = Gtk.Image()
-    #    image_area = Gtk.Box()
-    #    image.set_from_file('/tmp/xkcd.png')
-    #    image_area.add(image)
-    #    image_area.show_all()
+        self.button.connect ("clicked", self.update_image)
 
+        self.main_box.add(self.image_area)
+        self.main_box.add(self.button)
+        self.add(self.main_box)
+
+    def update_image(win,butt):
+        getRandomComic().download(output='/tmp/',outputFile='xkcd.png')
+        win.image.set_from_file('/tmp/xkcd.png')
+
+comic = xkcd.getLatestComic()
+comic.download(output="/tmp/",outputFile="xkcd.png")
 win = myWindow()
 win.connect("delete-event", Gtk.main_quit)
 #win.add(box)
