661b5c9a44ccec0708898b389526b7311ac557d1
Brigdan <kacperdev@gmail.com>
2017-06-23 12:47:35 +0100

    Added next button

--- xkcdb.py
+++ xkcdb.py
@@ -7,6 +7,7 @@ gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
 
 rand = SystemRandom()
+number = 0 #Default
 
 def getRandomComic():
     number = rand.randint(1,xkcd.getLatestComicNum())
@@ -21,36 +22,35 @@ class myWindow(Gtk.Window):
         self.image = Gtk.Image()
         self.image_area = Gtk.Box()
 
+        self.button = Gtk.Button(label="random xkcd")
 
         self.image.set_from_file('/tmp/xkcd.png')
         self.image_area.add(self.image)
         self.image_area.show_all()
 
-        self.button = Gtk.Button(label="random xkcd")
+        self.button.connect ("clicked", self.update_image)
 
         self.main_box.add(self.image_area)
         self.main_box.add(self.button)
         self.add(self.main_box)
 
-        self.button.connect ("clicked", self.update_image)
-        self.main_box.pack_start(button, True, True, 0)
-        button = Gtk.Button.new_with_label("Click Me")
-        button.connect("clicked", self.on_click_me_clicked)
+        button = Gtk.Button.new_with_label("Next Comic")
+        button.connect("clicked", self.on_next_clicked)
         self.main_box.pack_start(button, True, True, 0)
 
-        button = Gtk.Button.new_with_mnemonic("_Open")
-        button.connect("clicked", self.on_open_clicked)
-        self.main_box.pack_start(button, True, True, 0)
+    #    button = Gtk.Button.new_with_mnemonic("_Open")
+    #    button.connect("clicked", self.on_open_clicked)
+    #    self.main_box.pack_start(button, True, True, 0)
 
         button = Gtk.Button.new_with_mnemonic("_Close")
         button.connect("clicked", self.on_close_clicked)
         self.main_box.pack_start(button, True, True, 0)
 
-    def on_click_me_clicked(self, button):
-        print("\"Click me\" button was clicked")
+    def on_next_clicked(self, button):
+        xkcd.getComic((number + 1), silent=True)
 
-    def on_open_clicked(self, button):
-        print("\"Open\" button was clicked")
+    #def on_open_clicked(self, button):
+    #    print("\"Open\" button was clicked")
 
     def on_close_clicked(self, button):
         print("Closing application")
