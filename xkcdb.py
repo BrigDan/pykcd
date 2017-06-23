aef18b662bcc8fbba7e2793a082f87709c539dbf
brig <brig@cyberia>
2017-06-23 12:08:32 +0100

    More buttons added to be re-assigned

--- xkcdb.py
+++ xkcdb.py
@@ -15,21 +15,46 @@ def getRandomComic():
 class myWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="XKCD Browser")
+
         self.main_box = Gtk.VBox()
 
         self.image = Gtk.Image()
         self.image_area = Gtk.Box()
-        self.button = Gtk.Button(label="random xkcd")
+
 
         self.image.set_from_file('/tmp/xkcd.png')
         self.image_area.add(self.image)
         self.image_area.show_all()
 
-        self.button.connect ("clicked", self.update_image)
+        self.button = Gtk.Button(label="random xkcd")
 
         self.main_box.add(self.image_area)
         self.main_box.add(self.button)
         self.add(self.main_box)
+
+        self.button.connect ("clicked", self.update_image)
+        self.main_box.pack_start(button, True, True, 0)
+        button = Gtk.Button.new_with_label("Click Me")
+        button.connect("clicked", self.on_click_me_clicked)
+        self.main_box.pack_start(button, True, True, 0)
+
+        button = Gtk.Button.new_with_mnemonic("_Open")
+        button.connect("clicked", self.on_open_clicked)
+        self.main_box.pack_start(button, True, True, 0)
+
+        button = Gtk.Button.new_with_mnemonic("_Close")
+        button.connect("clicked", self.on_close_clicked)
+        self.main_box.pack_start(button, True, True, 0)
+
+    def on_click_me_clicked(self, button):
+        print("\"Click me\" button was clicked")
+
+    def on_open_clicked(self, button):
+        print("\"Open\" button was clicked")
+
+    def on_close_clicked(self, button):
+        print("Closing application")
+        Gtk.main_quit()
 
     def update_image(win,butt):
         getRandomComic().download(output='/tmp/',outputFile='xkcd.png')
@@ -37,6 +62,7 @@ class myWindow(Gtk.Window):
 
 comic = xkcd.getLatestComic()
 comic.download(output="/tmp/",outputFile="xkcd.png")
+
 win = myWindow()
 win.connect("delete-event", Gtk.main_quit)
 #win.add(box)
