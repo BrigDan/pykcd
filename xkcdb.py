96494be6636e789d2b2b35226bd2a78493d6ac2b
faissaloo <faissaloo@gmail.com>
2017-06-23 13:16:16 +0100

    Fixed next button and improved code layout

--- xkcdb.py
+++ xkcdb.py
@@ -9,62 +9,63 @@ from gi.repository import Gtk
 rand = SystemRandom()
 number = 0 #Default
 
-def getRandomComic():
-    number = rand.randint(1,xkcd.getLatestComicNum())
-    return xkcd.Comic(number)
-
 class myWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="XKCD Browser")
+
+        #self.cur_comic stores the number of the latest comic
+        self.cur_comic=xkcd.getLatestComicNum()
 
         self.main_box = Gtk.VBox()
 
         self.image = Gtk.Image()
         self.image_area = Gtk.Box()
 
-        self.button = Gtk.Button(label="random xkcd")
+        self.rand_btn = Gtk.Button.new_with_label("Random XKCD")
 
         self.image.set_from_file('/tmp/xkcd.png')
         self.image_area.add(self.image)
         self.image_area.show_all()
 
-        self.button.connect ("clicked", self.update_image)
+        self.rand_btn.connect ("clicked", self.on_random_clicked)
 
         self.main_box.add(self.image_area)
-        self.main_box.add(self.button)
+        self.main_box.add(self.rand_btn)
         self.add(self.main_box)
 
-        button = Gtk.Button.new_with_label("Next Comic")
-        button.connect("clicked", self.on_next_clicked)
-        self.main_box.pack_start(button, True, True, 0)
+        self.nxt_btn = Gtk.Button.new_with_label("Next Comic")
+        self.nxt_btn.connect("clicked", self.on_next_clicked)
+        self.main_box.pack_start(self.nxt_btn, True, True, 0)
 
     #    button = Gtk.Button.new_with_mnemonic("_Open")
     #    button.connect("clicked", self.on_open_clicked)
     #    self.main_box.pack_start(button, True, True, 0)
 
-        button = Gtk.Button.new_with_mnemonic("_Close")
-        button.connect("clicked", self.on_close_clicked)
-        self.main_box.pack_start(button, True, True, 0)
+        self.close_btn = Gtk.Button.new_with_mnemonic("_Close")
+        self.close_btn.connect("clicked", self.on_close_clicked)
+        self.main_box.pack_start(self.close_btn, True, True, 0)
+
+        self.connect("delete-event", Gtk.main_quit)
+        self.show_all()
 
     def on_next_clicked(self, button):
-        xkcd.getComic((number + 1), silent=True)
+        self.cur_comic+=1
+        self.update_image()
 
-    #def on_open_clicked(self, button):
-    #    print("\"Open\" button was clicked")
+    def on_random_clicked(self,button):
+        self.cur_comic=rand.randint(1,xkcd.getLatestComicNum())
+        self.update_image()
 
     def on_close_clicked(self, button):
-        print("Closing application")
         Gtk.main_quit()
 
-    def update_image(win,butt):
-        getRandomComic().download(output='/tmp/',outputFile='xkcd.png')
-        win.image.set_from_file('/tmp/xkcd.png')
+    #Whenever we want to update the comic displayed we'll use this
+    def update_image(self):
+        xkcd.getComic(self.cur_comic).download(output='/tmp/',outputFile='xkcd.png')
+        self.image.set_from_file('/tmp/xkcd.png')
 
 comic = xkcd.getLatestComic()
 comic.download(output="/tmp/",outputFile="xkcd.png")
 
-win = myWindow()
-win.connect("delete-event", Gtk.main_quit)
-#win.add(box)
-win.show_all()
+myWindow()
 Gtk.main()
